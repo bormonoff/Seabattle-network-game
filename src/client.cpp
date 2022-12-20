f68b5305447a00dd64cd56c9df16657b9d8a6fd3
Bormon <botmon_off@protonmail.com>
2022-12-20 09:47:48 +0300

    First syntax added

---
+++ src/client.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <boost/asio.hpp>
+
+
+using namespace std;
+namespace net = boost::asio;
+using net::ip::tcp;
+
+
+int main(){
+    net::io_context io;
+
+    cout<<"waiting for connetion"<<endl;
+
+    boost::system::error_code ex;
+    tcp::socket socket{io};
+    socket.connect(tcp::endpoint(net::ip::make_address("127.0.0.1", ex),3333));
+
+    if(ex){
+        cout<<"error connect"<<endl;
+      
+    }
+
+   socket.write_some(net::buffer("Client, aboba"));
+
+   net::streambuf stream_buf;
+   net::read_until(socket, stream_buf, '\0', ex);
+   std::string client_data{std::istreambuf_iterator<char>(&stream_buf),
+                        std::istreambuf_iterator<char>()};
+
+    cout<<client_data<<endl; 
+
+    return 0;
+}
